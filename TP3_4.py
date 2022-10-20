295b5a3081b4591bb0983c1372ab78e72401ffbb
Benjamin Peronne <50184355+BenjaminPeronne@users.noreply.github.com>
2022-10-20 11:21:00 -0400

    Update `TP3_4.py`
    Exercice done

--- TP3_4.py
+++ TP3_4.py
@@ -7,3 +7,35 @@
 #  */
 
 # Objectif : Suite de Syracuse
+
+def etapeSuivante(p_u): # Etape suivante de Syracuse
+    if p_u % 2 == 0:
+        return p_u // 2 # Si u est pair
+    else:
+        return 3 * p_u + 1 # Si u est impair
+
+
+def vol(p_uinit): # Vol de Syracuse
+    liste = []
+    u = p_uinit
+
+    while u != 1:
+        liste.append(u)
+        u = etapeSuivante(u)
+
+    liste.append(u)
+    return liste
+
+
+def main():
+    uinit = int(input("Entrez un entier initial strictement positif : "))
+    if (uinit <= 0):
+        print("=> Erreur : uinit doit être strictement positif")
+        while (uinit <= 0):
+            uinit = int(input(">>> Entrez un entier initial strictement positif : "))
+    else:
+        print(vol(uinit))
+
+
+if __name__ == "__main__":
+    main()
